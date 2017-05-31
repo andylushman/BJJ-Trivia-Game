daf84396c54eaa4889881e56ad4ba0e10e0930fc
Andy Lushman <andylushman@gmail.com>
2017-05-30 19:08:49 -0600

    Created start button

---
+++ assets/javascript/app.js
@@ -0,0 +1,17 @@
+//Global Variables
+var initialScreen;
+
+
+
+//Functions
+
+  function startScreen(){
+    initialScreen = "<p class='text-center main-button'><a class='btn btn-primary btn-lg' href='#'>Start Fight</a></p>";
+
+    $(".main-area").html(initialScreen);
+  }
+  //show the start button
+
+
+//Main Process
+startScreen();
