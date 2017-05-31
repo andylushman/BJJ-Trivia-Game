7b7dfdec28fb7ced707ddde14b9ed4a80729cf2c
Andy Lushman <andylushman@gmail.com>
2017-05-30 19:54:59 -0600

    Created object trivia

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -3,8 +3,16 @@ var trivia = {
   initialScreen: "",
   correctCounter: 0,
   inCorrectCounter: 0,
+  unAnsweredCounter: 0,
+  clickSound: new Audio("./assets/sounds/button-click.wav"),
+  gameHTML: "",
+  questionsArray: ["Question 1", "questions 2", "Question 3", "question 4", "question 5"],
+  answerArray: [
+                ["option 1", "option 2", "option 3"], ["option 1", "option 2", "option 3"], ["option 1", "option 2", "option 3"], ["option 1", "option 2", "option 3"], ["option 1", "option 2", "option 3"],],
+  imageArray: [],
+  clock: "",
 
-}
+};
 
 
 
@@ -12,12 +20,17 @@ var trivia = {
 
 //Functions
 
-  function startScreen(){
-    //Create the start button
-    trivia.initialScreen = "<p class='text-center main-button'><a class='btn btn-primary btn-lg' href='#'>Luta!</a></p>";
-    //Add Start button to main-area
-    $(".main-area").html(trivia.initialScreen);
+function startScreen(){
+  //Create the start button
+  trivia.initialScreen = "<p class='text-center main-button'><a class='btn btn-primary btn-lg' href='#'>Luta!</a></p>";
+  //Add Start button to main-area
+  $(".main-area").html(trivia.initialScreen);
   }
+
+//generateHTML() is triggered by the start button, and generates the HTML
+function generateHTML(){
+  trivia.gameHTML = ""
+}
 
 
 
